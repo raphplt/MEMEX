e8570750c02fdfd71271b3d4bca6a496d32d13a4
raphaeltkorp <raphael@tkorp.com>
2025-12-09 23:31:57 +0100

    pnpm -> npm

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { FeedModule } from "./feed/feed.module";
 import { WikiModule } from "./wiki/wiki.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
@@ -6,15 +7,22 @@ import { Card } from "./cards/card.entity";
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: "postgres",
-			host: "localhost",
-			port: 5432,
-			username: "user",
-			password: "password",
-			database: "memex",
-			autoLoadEntities: true,
-			synchronize: true,
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: "postgres",
+				host: configService.get<string>("DB_HOST"),
+				port: configService.get<number>("DB_PORT"),
+				username: configService.get<string>("DB_USERNAME"),
+				password: configService.get<string>("DB_PASSWORD"),
+				database: configService.get<string>("DB_NAME"),
+				autoLoadEntities: true,
+				synchronize: true,
+			}),
+			inject: [ConfigService],
 		}),
 		TypeOrmModule.forFeature([Card]),
 		FeedModule,
